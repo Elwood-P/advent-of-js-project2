0cb6d85de235c3c5f2ca645a15f5f87e7ab63d89
Elwood-P <info@pjlittlewood.co.uk>
2023-02-14 12:29:21 +0000

    Add cart is empty message

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,10 +7,12 @@ import CartTotals from './CartTotals';
 
 function Cart() {
   const { cart } = useContext(CartContext);
+  const isCartEmpty = !cart.length;
 
   return (
     <Panel title="Your Cart">
       <div className="cart">
+        {isCartEmpty && <p className="empty">Your cart is empty.</p>}
         <ul className="cart-summary">
           {cart.map((cartItem) => {
             return <CartItem key={cartItem.name} cartItem={cartItem} />;
